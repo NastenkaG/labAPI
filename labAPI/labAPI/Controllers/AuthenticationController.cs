91e4708f3ffcc17dff29a39b79886f5adb2af8ce
anastasia <anastasia.guznova@yandex.ru>
2022-12-14 16:22:51 +0300

    добавлена регистрация

--- labAPI/labAPI/Controllers/AuthenticationController.cs
+++ labAPI/labAPI/Controllers/AuthenticationController.cs
@@ -38,5 +38,23 @@ namespace labAPI.Controllers
             }
             return Ok(new { Token = await _authManager.CreateToken() });
         }
+
+        [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+        {
+            var user = _mapper.Map<User>(userForRegistration);
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+            await _userManager.AddToRolesAsync(user, userForRegistration.Roles);
+            return StatusCode(201);
+        }
     }
 }
